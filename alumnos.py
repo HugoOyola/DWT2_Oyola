b0c316cce632b157137cceb06f409d09cfe04531
Hugo Oyola <hugo.ham@outlook.com>
2023-11-27 17:08:19 -0500

    Agregando comentarios

--- alumnos.py
+++ alumnos.py
@@ -1,17 +1,24 @@
+# Definir la clase Alumno
 class Alumno:
-  def __init__(self, nombre, apellido, edad, nacionalidad, nota=0):
-      self.nombre = nombre
-      self.apellido = apellido
-      self.edad = edad
-      self.nacionalidad = nacionalidad
-      self.nota = nota
+    # Constructor de la clase
+    def __init__(self, nombre, apellido, edad, nacionalidad):
+        self.nombre = nombre # Atributo nombre
+        self.apellido = apellido # Atributo apellido
+        self.edad = edad # Atributo edad
+        self.nacionalidad = nacionalidad # Atributo nacionalidad
+        self.nota = None # Atributo nota, inicialmente None
 
-  def leerNota(self):
-      return self.nota
+    # Método para leer la nota del alumno
+    def leerNota(self):
+        return self.nota
 
-  def registrarNota(self, notaAlumno):
-      if 0 <= notaAlumno <= 20:
-          self.nota = notaAlumno
-          print(f"Nota registrada para {self.nombre} {self.apellido}")
-      else:
-          print("La nota debe estar en un rango de 0 a 20. Inténtelo de nuevo.")
+    # Método para registrar la nota del alumno
+    def registrarNota(self, notaAlumno):
+        # Validar que la nota sea un número entre 0 y 20
+        if isinstance(notaAlumno, int) or isinstance(notaAlumno, float):
+            if notaAlumno >= 0 and notaAlumno <= 20:
+                self.nota = notaAlumno # Asignar la nota al atributo
+            else:
+                print("La nota debe estar entre 0 y 20") # Mostrar mensaje de error
+        else:
+            print("La nota debe ser un número") # Mostrar mensaje de error
--- registroNotas.py
+++ registroNotas.py
@@ -1,69 +1,90 @@
-import pickle
+# Importar la clase Alumno y la librería pickle
 from alumnos import Alumno
+import pickle
 
+# Crear una lista vacía para almacenar los objetos Alumno
+listaAlumnos = []
 
-def main():
-    alumnos_registrados = []
+# Desplegar un mensaje inicial
+print("Bienvenidos al registro de notas")
 
-    print("Bienvenidos al registro de notas")
+# Crear un bucle para esperar el ingreso de comandos
+while True:
+    # Pedir el ingreso de un comando
+    comando = input("Ingrese comando (R, C, P, S, X): ")
 
-    while True:
-        comando = input("Ingrese comando (R, C, P, S, X): ").upper()
+    # Si el comando es R, registrar un alumno
+    if comando == "R":
+        # Pedir los datos del alumno
+        nombre = input("Ingrese el nombre del alumno: ")
+        apellido = input("Ingrese el apellido del alumno: ")
+        edad = input("Ingrese la edad del alumno: ")
+        nacionalidad = input("Ingrese la nacionalidad del alumno: ")
 
-        if comando == "R":
-            nombre = input("Ingrese nombre del alumno: ")
-            apellido = input("Ingrese apellido del alumno: ")
-            edad = int(input("Ingrese edad del alumno: "))
-            nacionalidad = input("Ingrese nacionalidad del alumno: ")
+        # Crear un objeto Alumno con los datos ingresados
+        alumno = Alumno(nombre, apellido, edad, nacionalidad)
 
-            nuevo_alumno = Alumno(nombre, apellido, edad, nacionalidad)
-            alumnos_registrados.append(nuevo_alumno)
+        # Agregar el objeto Alumno a la lista
+        listaAlumnos.append(alumno)
 
-        elif comando == "C":
-            for alumno in alumnos_registrados:
-                nota_valida = False
-                while not nota_valida:
-                    try:
-                        nota = int(
-                            input(
-                                f"Ingrese nota para {alumno.nombre} {alumno.apellido}: "
-                            )
-                        )
-                        alumno.registrarNota(nota)
-                        nota_valida = True
-                    except ValueError:
-                        print("Ingrese un valor numérico válido.")
+    # Si el comando es C, calificar a los alumnos
+    elif comando == "C":
+        # Recorrer la lista de alumnos
+        for alumno in listaAlumnos:
+            # Pedir el ingreso de la nota para cada alumno
+            nota = input(f"Alumno {alumno.nombre} {alumno.apellido}, Ingrese nota: ")
 
-        elif comando == "P":
-            if alumnos_registrados:
-                promedio = sum(
-                    alumno.leerNota() for alumno in alumnos_registrados
-                ) / len(alumnos_registrados)
-                print(
-                    f"El promedio de notas para {len(alumnos_registrados)} alumnos es: {promedio:.2f}"
-                )
-            else:
-                print("No hay alumnos registrados.")
+            # Intentar convertir la nota a un número
+            try:
+                nota = float(nota)
+            except:
+                # Si no se puede convertir, asignar None
+                nota = None
 
-        elif comando == "S":
-            if alumnos_registrados:
-                suma_notas = sum(alumno.leerNota()
-                                 for alumno in alumnos_registrados)
-                print(
-                    f"La suma de notas de {len(alumnos_registrados)} alumnos es: {suma_notas}"
-                )
-            else:
-                print("No hay alumnos registrados.")
+            # Registrar la nota del alumno usando el método de la clase
+            alumno.registrarNota(nota)
 
-        elif comando == "X":
-            with open("alumnos_data.pkl", "wb") as file:
-                pickle.dump(alumnos_registrados, file)
-            print("Programa finalizado.")
-            break
+    # Si el comando es P, obtener el promedio de notas
+    elif comando == "P":
+        # Crear una variable para almacenar la suma de notas
+        suma = 0
 
-        else:
-            print("Comando no reconocido. Inténtelo de nuevo.")
+        # Recorrer la lista de alumnos
+        for alumno in listaAlumnos:
+            # Sumar la nota de cada alumno a la variable suma
+            suma += alumno.leerNota()
 
+        # Calcular el promedio de notas dividiendo la suma por la cantidad de alumnos
+        promedio = suma / len(listaAlumnos)
 
-if __name__ == "__main__":
-    main()
+        # Mostrar el mensaje con el promedio de notas
+        print(f"El promedio de notas para {len(listaAlumnos)} alumnos es: {promedio}")
+
+    # Si el comando es S, obtener la suma de notas
+    elif comando == "S":
+        # Crear una variable para almacenar la suma de notas
+        suma = 0
+
+        # Recorrer la lista de alumnos
+        for alumno in listaAlumnos:
+            # Sumar la nota de cada alumno a la variable suma
+            suma += alumno.leerNota()
+
+        # Mostrar el mensaje con la suma de notas
+        print(f"La suma de notas de {len(listaAlumnos)} alumnos es: {suma}")
+
+    # Si el comando es X, terminar el programa
+    elif comando == "X":
+        # Guardar los objetos Alumno en un archivo usando pickle
+        with open("alumnos.pkl", "wb") as archivo:
+            pickle.dump(listaAlumnos, archivo)
+
+        # Mostrar un mensaje de despedida
+        print("Gracias por usar el registro de notas. Hasta pronto.")
+
+        # Romper el bucle
+        break
+
+    # Si el comando no es válido, mostrar un mensaje de error
+    else:
+        print("Comando no válido. Por favor, ingrese R, C, P, S o X.")
